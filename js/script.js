22cdb5bf280bfae50450d0e5d888c4754f88c3d4
Firefly718 <System.BLA.16@yandex.ru>
2020-09-01 18:24:23 +0300

    fixed text-oferflow in news section

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
   $('.slider-two').slick({
       dots: true,
       slidesToShow: 3,
-      autoplay: true,
+      autoplay: false,
       autoplaySpeed: 4000,
       responsive: [
         {
@@ -243,6 +243,16 @@ $(document).ready(function(){
     $("#header > nav > div.nav__links").toggle('slow', 'swing');
     // $("#header > nav > ul").toggleClass("social-left");
   });
+});
+
+// ellipsis
+$('.foo').ellipsis();
+
+// ellipsis settings
+$('.foo').ellipsis({
+  lines: 3,             // force ellipsis after a certain number of lines. Default is 'auto'
+  ellipClass: 'ellip',  // class used for ellipsis wrapper and to namespace ellip line
+  responsive: true      // set to true if you want ellipsis to update on window resize. Default is false
 });
 
 
